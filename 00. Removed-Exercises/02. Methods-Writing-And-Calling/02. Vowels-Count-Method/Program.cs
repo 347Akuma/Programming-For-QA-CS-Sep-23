a802c4891dc71c457e115a056a1a7b3c2ca7140b
Alexander Dimitrov <adimitrovx@gmail.com>
2023-09-27 01:18:44 +0300

    first steps in methods

---
+++ 00. Removed-Exercises/02. Methods-Writing-And-Calling/02. Vowels-Count-Method/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _02._Vowels_Count_Method
+{
+    internal class Program
+    {
+
+        static int calculateVowelsCount(string input)
+        {
+            int count = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = char.ToLower(input[i]);
+                switch (c)
+                {
+                    case 'a':
+                    case 'e':
+                    case 'o':
+                    case 'u':
+                    case 'i':
+                        count++;
+                        break;
+                }            
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine() ?? "0";
+
+            int vowelCount = calculateVowelsCount(input);
+
+            Console.WriteLine(vowelCount);
+        }
+    }
+}
---
+++ 00. Removed-Exercises/02. Methods-Writing-And-Calling/03. Characters-In-Range-Method/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _03._Characters_In_Range
+{
+    internal class Program
+    {
+        static char CharactersInRange(char char1, char char2)
+        {
+            char1 = asci
+        }
+        static void Main(string[] args)
+        {
+            char char1 = char.Parse(Console.ReadLine());
+            char char2 = char.Parse(Console.ReadLine());
+
+
+
+            
+            Console.Write("Hello, World!");
+        }
+    }
+}
