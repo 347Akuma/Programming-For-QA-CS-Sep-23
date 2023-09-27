9aba0b1c7315051cdca68555512f5d81630ac20d
Alexander Dimitrov <adimitrovx@gmail.com>
2023-09-27 13:46:36 +0300

    some edits

---
+++ 00. Removed-Exercises/02. Methods-Writing-And-Calling/03.  Chars-In-Range/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _03.__Chars_In_Range
+{
+    internal class Program
+    {
+        // Method to print characters between two characters based on ASCII
+        static void Main()
+        {
+            char firstChar = Console.ReadLine()[0]; // Read the first character            
+            char secondChar = Console.ReadLine()[0]; // Read the second character
+
+            PrintCharactersInRange(firstChar, secondChar); // Call the method
+        }
+
+        static void PrintCharactersInRange(char start, char end)
+        {
+            // Swap characters if end < start
+            if (end < start)
+            {
+                char temp = start;
+                start = end;
+                end = temp;
+            }
+
+            // Print characters from start to end
+            for (char i = (char)(start + 1); i < end; i++)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine(); // Print a newline after all characters are printed
+        }
+    }
+}
---
+++ 00. Removed-Exercises/02. Methods-Writing-And-Calling/Methods-Writing-And-Calling/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Methods_Writing_And_Calling
+{
+    internal class Program
+    {
+        static int SmallestOfThreeNumbers(int num1, int num2, int num3)
+        {
+            return Math.Min(Math.Min(num1, num2), num3);
+        }
+
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int smallest = SmallestOfThreeNumbers(num1, num2, num3);
+
+            Console.WriteLine(smallest);
+
+        }
+    }
+}
