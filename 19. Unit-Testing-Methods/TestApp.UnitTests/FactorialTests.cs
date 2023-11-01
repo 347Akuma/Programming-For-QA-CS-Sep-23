116ff8dfe1d59ff57d26c16659d66ba2c29df715
Alexander Dimitrov <adimitrovx@gmail.com>
2023-11-01 15:28:01 +0200

    first UTs

---
+++ 19. Unit-Testing-Methods/TestApp.UnitTests/FactorialTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestApp.UnitTests;
+
+public class FactorialTests
+{
+    [Test]
+    public void CalculateFactorial_InputZero_ReturnsOne()
+    {
+        //Arrange
+        int n = 0;
+
+        //Act
+        int output = Factorial.CalculateFactorial(n);
+
+        //Assert
+        Assert.AreEqual(1, output);
+    }
+
+    [Test]
+    public void CalculateFactorial_InputPositiveNumber_ReturnsCorrectFactorial()
+    {
+        //Arrange
+        int n = 2;
+
+        //Act
+        int output = Factorial.CalculateFactorial(n);
+
+        //Assert
+        Assert.AreEqual(2, output);
+    }
+
+    [Test]
+    public void CalculateFactorial_InputNegativeNumber_ThrowsException()
+    {
+        //Arrange
+        int n = -2;
+
+        //Act + Assert (Assert.Throws<exception>(() => ClassName.MethodName(parameter));
+        Assert.Throws<ArgumentOutOfRangeException>(() => Factorial.CalculateFactorial(n));
+
+    }
+}
+
---
+++ 19. Unit-Testing-Methods/TestApp.UnitTests/StringReverseTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace TestApp.UnitTests;
+
+public class StringReverseTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_Reverse_WhenGivenEmptyString_ReturnsEmptyString()
+    {
+        // Arrange
+        string input = "";
+
+        // Act
+       string actual = StringReverse.Reverse(input);
+
+        // Assert
+       Assert.AreEqual("", actual);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenSingleCharacterString_ReturnsSameCharacter()
+    {
+        //Arrange
+        string input = "a";
+
+       //Act
+       string actual = StringReverse.Reverse(input);
+
+       //Assert
+       Assert.That(actual, Is.EqualTo(input));
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenNormalString_ReturnsReversedString()
+    {
+        //Arrange
+        string input = "ABCD";
+
+        //Act
+        string actual = StringReverse.Reverse(input);
+
+        //Assert
+        Assert.That(actual, Is.EqualTo(input.Reverse()));
+    }
+}
